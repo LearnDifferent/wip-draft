b04be197078e230aef243a74210081db1470ca98
zhou <imzhouhepeng@outlook.com>
2021-09-06 19:07:50 +0800

    update

--- src/main/java/com/github/learndifferent/mtm/annotation/general/log/SystemLog.java
+++ src/main/java/com/github/learndifferent/mtm/annotation/general/log/SystemLog.java
@@ -3,6 +3,8 @@ package com.github.learndifferent.mtm.annotation.general.log;
 import com.github.learndifferent.mtm.constant.enums.OptsType;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -11,6 +13,7 @@ import java.lang.annotation.Target;
  * @author zhou
  * @date 2021/09/05
  */
+@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface SystemLog {
 
